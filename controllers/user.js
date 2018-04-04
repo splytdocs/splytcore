3769099cf0a53fe43a7080de5b351546250fd5f0
Dipen Joshi <dipenjoshi@users.noreply.github.com>
2018-04-04 13:02:20 -0700

    increased getting wallet balance

--- controllers/user.js
+++ controllers/user.js
@@ -169,7 +169,7 @@ exports.accountGet = function(req, res, next) {
             user.walletBalance = 0;
             return send200(res, scrubUser(user));
           }
-        }, 1000);
+        }, 2000);
         
       }
     });
